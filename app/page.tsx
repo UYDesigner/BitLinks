033a800d2e7d89604eed75dcb6d98a3681336ddd
urvashiyadav <urvashiy678@gmail.com>
2025-06-05 19:41:28 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Home() {
   const btn = [
@@ -12,12 +14,26 @@ export default function Home() {
       <section className="grid grid-cols-1 sm:grid-cols-2 max-w-[1500px] mx-auto min-h-[90vh] px-4 py-10 sm:py-0">
         {/* Left Section */}
         <div className="flex flex-col order-2 sm:order-1 justify-center gap-6">
-          <h4 className="font-bold text-[24px] sm:text-[30px] text-[#033f47] leading-tight">
+          <h4 className="font-bold text-[28px] sm:text-[34px] text-[#033f47] leading-tight">
             The best URL shortener in the market
           </h4>
           <p className="text-base sm:text-lg text-gray-700">
             We are the most straightforward and reliable URL shortener in the world. We understand the needs of users and provide seamless performance without any hassle.
           </p>
+          <div className="text-[#033f47] font-extrabold text-2xl sm:text-2xl h-[40px] sm:h-[50px] animate-pulse">
+            <span className="">
+              <Typewriter
+                words={["Privacy Focused", "URL Shortener", "Fast & Easy", "Link Optimizer", "BitLinks Tool"]}
+                loop={0}
+                cursor
+                cursorStyle="|"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1500}
+              />
+            </span>
+          </div>
+
           <div className="flex flex-wrap gap-3 font-[500]">
             {btn.map((button, idx) => (
               <Link href={button.path} key={idx}>
@@ -38,7 +54,7 @@ export default function Home() {
               fill
               className="object-contain mix-blend-darken"
               priority
-              
+
             />
           </div>
         </div>
